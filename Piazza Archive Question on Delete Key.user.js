2bd1a993a6311be48691ea5ea646892472e6e070
Zach Hardesty <zachhardesty7@gmail.com>
2019-02-14 20:06:19 -0600

    implement waitforkeyelements to ensure items are captured

--- Piazza Archive Question on Delete Key.user.js
+++ Piazza Archive Question on Delete Key.user.js
@@ -1,5 +1,5 @@
 /* eslint-env browser, jquery, greasemonkey */
-/* global P */
+/* global P waitForKeyElements */
 
 // ==UserScript==
 // @name        Piazza Bind Delete Key to Archive
@@ -7,11 +7,15 @@
 // @description bind the delete key to quickly archive posts
 // @include     https://piazza.com/class/*
 // @version     1.0.0
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.js
+// @require     https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-// FIXME: doesn't work on first element?
+// FIXME: can sometimes archive the wrong post if other keys are pressed before delete
 
-(function f() {
+function f(jNode) {
+  const el = jNode[0]
+
   // curried function to call on delete press event listener
   // calls function as if archive dropdown menu item was pressed
   const archiveItem = archive => (e) => {
@@ -19,15 +23,12 @@
   }
 
   // set all initial event listeners
-  const setListeners = () => {
-    const els = document.querySelectorAll('.feed_item')
-    els.forEach((el, i) => {
-      const archive = el.querySelector('.feed_item_dropdown_selector .archive_action').onclick
+  const setListeners = (item) => {
+    const archive = item.querySelector('.feed_item_dropdown_selector .archive_action').onclick
 
-      el.tabIndex = '0'
-      el.addEventListener('keydown', archiveItem(archive))
-      el.addEventListener('click', resetListeners(i))
-    })
+    item.tabIndex = '0'
+    item.addEventListener('keydown', archiveItem(archive))
+    item.addEventListener('click', resetListeners(i))
   }
 
   // curried function to call on click event listener
@@ -35,23 +36,31 @@
   // interval until the tabIndex of the updated element is set
   const resetListeners = i => () => {
     const ensureIndex = setInterval(() => {
-      const els = document.querySelectorAll('.feed_item')
-      els.forEach((el) => { el.tabIndex = '0' })
-      const el = els[i]
+      const items = document.querySelectorAll('.feed_item')
+      items.forEach((n) => { n.tabIndex = '0' })
+      const item = items[i]
 
-      if (el.tabIndex === 0) {
-        const archive = el.querySelector('.feed_item_dropdown_selector .archive_action')
+      if (item.tabIndex === 0 || item.tabIndex === '0') {
+        const archive = item.querySelector('.feed_item_dropdown_selector .archive_action')
 
-        el.removeEventListener('keydown', archiveItem(archive))
-        el.addEventListener('keydown', archiveItem(archive))
-        el.addEventListener('click', resetListeners(i))
-        el.focus()
+        item.removeEventListener('keydown', archiveItem(archive))
+        item.addEventListener('keydown', archiveItem(archive))
+        item.addEventListener('click', resetListeners(i))
+        item.focus()
 
         clearInterval(ensureIndex)
       }
     }, 100)
   }
 
-  // establish initial listeners
-  setListeners()
-})()
+  // find the index of the current item
+  let i = 0
+  document.querySelectorAll('.feed_item').forEach((el2, k) => {
+    if (el2.id === el.id) i = k
+  })
+
+  setListeners(el)
+}
+
+// instantiate function listeners
+waitForKeyElements('.feed_item', f)
