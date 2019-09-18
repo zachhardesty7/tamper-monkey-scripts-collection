7bf3145e0a70301ae9f196d718215d74efa03116
Zach Hardesty <zachhardesty7@gmail.com>
2019-09-18 11:41:04 -0500

    fix and simplify piazza script

--- piazza-archive-with-delete-key.user.js
+++ piazza-archive-with-delete-key.user.js
@@ -5,7 +5,7 @@
 // @description  bind the delete key to quickly archive posts
 // @copyright    2019, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      1.0.0
+// @version      2.0.0
 
 // @homepageURL  https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/piazza-archive-with-delete-key.user.js
 // @homepageURL  https://openuserjs.org/scripts/zachhardesty7/Piazza_-_Archive_with_Delete_Key
@@ -15,54 +15,13 @@
 // @downloadURL  https://openuserjs.org/install/zachhardesty7/Piazza_-_Archive_with_Delete_Key.user.js
 
 // @include      https://piazza.com/class/*
-// @require      https://gist.githubusercontent.com/raw/ee7a6b80315148ad1fb6847e72a22313/
 // ==/UserScript==
 
-// FIXME: can sometimes archive the wrong post if other keys are pressed before delete
-
-window.onElementReady('.feed_item', false, (el) => {
-	// curried function to call on delete press event listener
-	// calls function as if archive dropdown menu item was pressed
-	const archiveItem = archive => (e) => {
-		if (e.key === 'Delete') window.P.feed.feedItemDropdownMenuCallback(archive)
+const delSelItem = (e) => {
+	if (e.key === 'Delete') {
+		const item = document.querySelector('.feed_item.selected')
+		P.feed.delFeedItem(item.id);
 	}
+}
 
-	// set all initial event listeners
-	const setListeners = (item) => {
-		const archive = item.querySelector('.feed_item_dropdown_selector .archive_action').onclick
-
-		item.tabIndex = '0' // eslint-disable-line no-param-reassign
-		item.addEventListener('keydown', archiveItem(archive))
-		item.addEventListener('click', resetListeners(i))
-	}
-
-	// curried function to call on click event listener
-	// pass through target el index and run
-	// interval until the tabIndex of the updated el is set
-	const resetListeners = i => () => {
-		const ensureIndex = setInterval(() => {
-			const items = document.querySelectorAll('.feed_item')
-			items.forEach((n) => { n.tabIndex = '0' }) // eslint-disable-line no-param-reassign
-			const item = items[i]
-
-			if (item.tabIndex === 0 || item.tabIndex === '0') {
-				const archive = item.querySelector('.feed_item_dropdown_selector .archive_action')
-
-				item.removeEventListener('keydown', archiveItem(archive))
-				item.addEventListener('keydown', archiveItem(archive))
-				item.addEventListener('click', resetListeners(i))
-				item.focus()
-
-				clearInterval(ensureIndex)
-			}
-		}, 100)
-	}
-
-	// find the index of the current item
-	let i = 0
-	document.querySelectorAll('.feed_item').forEach((el2, k) => {
-		if (el2.id === el.id) i = k
-	})
-
-	setListeners(el)
-})
+document.addEventListener('keydown', delSelItem)
