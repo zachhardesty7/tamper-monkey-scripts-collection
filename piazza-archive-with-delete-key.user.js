af99a890fac3059591753f3d880b3dba39b810f1
Zach Hardesty <zachhardesty7@gmail.com>
2019-09-25 11:57:53 -0500

    add piazza convo movement feat && update pkgs

--- piazza-archive-with-delete-key.user.js
+++ piazza-archive-with-delete-key.user.js
@@ -5,7 +5,7 @@
 // @description  bind the delete key to quickly archive posts
 // @copyright    2019, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      2.0.0
+// @version      2.1.0
 
 // @homepageURL  https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/piazza-archive-with-delete-key.user.js
 // @homepageURL  https://openuserjs.org/scripts/zachhardesty7/Piazza_-_Archive_with_Delete_Key
@@ -16,12 +16,49 @@
 
 // @include      https://piazza.com/class/*
 // ==/UserScript==
+/* global P */
 
-const delSelItem = (e) => {
+/**
+ * ret prev item in cur week if exists or back up and move to prev week to get last item
+ *
+ * @param {Element} el - target
+ * @returns {Element} result
+ */
+const getPrevItem = el => (
+	el.previousElementSibling ||
+	el.parentElement.parentElement.previousElementSibling.lastElementChild.lastElementChild
+)
+
+/**
+ * ret next item in cur week if exists or back up and move to next week to get first item
+ *
+ * @param {Element} el - target
+ * @returns {Element} result
+ */
+const getNextItem = el => (
+	el.nextElementSibling ||
+	el.parentElement.parentElement.nextElementSibling.lastElementChild.firstElementChild
+)
+
+const onKeydownHandler = (e) => {
+	// move to prev feed item
+	if (e.key === 'ArrowLeft' || e.key === 'ArrowUp') {
+		const item = document.querySelector('.feed_item.selected')
+		getPrevItem(item).click()
+	}
+
+	// move to next feed item
+	if (e.key === 'ArrowRight' || e.key === 'ArrowDown') {
+		const item = document.querySelector('.feed_item.selected')
+		getNextItem(item).click()
+	}
+
+	// del & move to next feed item
 	if (e.key === 'Delete') {
 		const item = document.querySelector('.feed_item.selected')
-		P.feed.delFeedItem(item.id);
+		getNextItem(item).click() // change view
+		P.feed.delFeedItem(item.id)
 	}
 }
 
-document.addEventListener('keydown', delSelItem)
+document.addEventListener('keydown', onKeydownHandler)
