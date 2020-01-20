eb2c78e84ad9ad191e2950921dcf3209749d8036
Zach Hardesty <zachhardesty7@gmail.com>
2020-01-20 13:31:05 -0600

    fix: bad download URL for docs

--- google-docs-force-center.user.js
+++ google-docs-force-center.user.js
@@ -7,14 +7,14 @@
 // @description  force center align of page display on Google Docs
 // @copyright    2019, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      0.1
+// @version      0.1.1
 
 // @homepageURL  https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/google-docs-force-center.user.js
 // @homepageURL  https://openuserjs.org/scripts/zachhardesty7/Google_Docs_-_Force_Center_Document_View
 // @supportURL   https://openuserjs.org/scripts/zachhardesty7/Google_Docs_-_Force_Center_Document_View/issues
 
 // @updateURL    https://openuserjs.org/meta/zachhardesty7/Google_Docs_-_Force_Center_Document_View.meta.js
-// @downloadURL  https://openuserjs.org/install/zachhardesty7/oogle_Docs_-_Force_Center_Document_View.user.js
+// @downloadURL  https://openuserjs.org/install/zachhardesty7/Google_Docs_-_Force_Center_Document_View.user.js
 
 // @match        https://docs.google.com/document/*
 // ==/UserScript==
