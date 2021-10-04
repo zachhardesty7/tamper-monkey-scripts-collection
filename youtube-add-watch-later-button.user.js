5e381e43e7a8102542e0ca78742632f29cf82f1d
Zach Hardesty <hello@zachhardesty.com>
2021-10-03 21:28:06 -0500

    feat(yt): rewrite for new style & fix class names

--- youtube-add-watch-later-button.user.js
+++ youtube-add-watch-later-button.user.js
@@ -5,7 +5,7 @@
 // @description  adds a new button next to like that quick adds / removes the active video from your "Watch later" playlist
 // @copyright    2019-2021, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      1.4.4
+// @version      1.5.0
 
 // @homepageURL  https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/youtube-add-watch-later-button.user.js
 // @homepageURL  https://openuserjs.org/scripts/zachhardesty7/YouTube_-_Add_Watch_Later_Button
@@ -23,9 +23,11 @@
 /* eslint-disable no-underscore-dangle */
 
 const BUTTONS_CONTAINER_ID = "top-level-buttons-computed"
-const ACTIVE_STYLE = "var(--yt-spec-call-to-action)"
-const INACTIVE_BUTTON_STYLE = "var(--yt-spec-icon-inactive)"
-const INACTIVE_TEXT_STYLE = "var(--yt-spec-text-secondary)"
+const SVG_ICON_CLASS = "style-scope yt-icon"
+const SVG_PATH_FILLED =
+  "M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10C22,6.48,17.52,2,12,2z M14.97,16.95L10,13.87V7h2v5.76 l4.03,2.49L14.97,16.95z"
+const SVG_PATH_HOLLOW =
+  "M14.97,16.95L10,13.87V7h2v5.76l4.03,2.49L14.97,16.95z M12,3c-4.96,0-9,4.04-9,9s4.04,9,9,9s9-4.04,9-9S16.96,3,12,3 M12,2c5.52,0,10,4.48,10,10s-4.48,10-10,10S2,17.52,2,12S6.48,2,12,2L12,2z"
 
 /**
  * Query for new DOM nodes matching a specified selector.
@@ -60,7 +62,7 @@ function addButton(buttons) {
   // normal action
   console.debug("no watch later button found, adding new button")
   const playlistSaveButton = document.querySelector(
-    "#top-level-buttons-computed > ytd-button-renderer:nth-child(4)"
+    "#top-level-buttons-computed > ytd-button-renderer:nth-child(5)"
   )
 
   // needed to force the node to load so we can determine if it's already in WL or not
@@ -108,14 +110,7 @@ function addButton(buttons) {
   svg.setAttribute("viewBox", "0 0 24 24")
   svg.setAttribute("preserveAspectRatio", "xMidYMid meet")
   svg.setAttribute("focusable", "false")
-  svg.setAttribute(
-    "class",
-    buttons.children[
-      buttons.children.length - 2
-    ].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute(
-      "class"
-    )
-  )
+  svg.setAttribute("class", SVG_ICON_CLASS)
   svg.setAttribute(
     "style",
     "pointer-events: none; display: block; width: 100%; height: 100%;"
@@ -123,39 +118,21 @@ function addButton(buttons) {
   icon.append(svg)
 
   const g = document.createElementNS("http://www.w3.org/2000/svg", "g")
-  g.setAttribute(
-    "class",
-    buttons.children[
-      buttons.children.length - 2
-    ].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute(
-      "class"
-    )
-  )
+  g.setAttribute("class", SVG_ICON_CLASS)
   svg.append(g)
 
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path")
-  path.setAttribute(
-    "class",
-    buttons.children[
-      buttons.children.length - 2
-    ].lastElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute(
-      "class"
-    )
-  )
-  path.setAttribute(
-    "d",
-    "M12 3.67c-4.58 0-8.33 3.75-8.33 8.33s3.75 8.33 8.33 8.33 8.33-3.75 8.33-8.33S16.58 3.67 12 3.67zm3.5 11.83l-4.33-2.67v-5h1.25v4.34l3.75 2.25-.67 1.08z"
-  )
+  path.setAttribute("class", SVG_ICON_CLASS)
+  path.setAttribute("d", SVG_PATH_HOLLOW)
   g.append(path)
 
   const text = document.createElement("yt-formatted-string")
   text.id = "text"
   link.append(text)
   text.className =
-    buttons.children[
-      buttons.children.length - 2
-    ].lastElementChild.lastElementChild.className
-  text.style.color = INACTIVE_TEXT_STYLE
+    buttons.children[buttons.children.length - 2].querySelector(
+      "#text"
+    ).className
   text.textContent = "later"
 
   let hasListener = false
@@ -168,22 +145,18 @@ function addButton(buttons) {
         console.debug("no click listener, adding new click listener")
         const watchLaterCheckbox = /** @type {HTMLInputElement} */ (checkbox)
 
-        container.style.color = watchLaterCheckbox?.checked
-          ? ACTIVE_STYLE
-          : INACTIVE_BUTTON_STYLE
-        text.style.color = watchLaterCheckbox?.checked
-          ? ACTIVE_STYLE
-          : INACTIVE_TEXT_STYLE
+        path.setAttribute(
+          "d",
+          watchLaterCheckbox?.checked ? SVG_PATH_FILLED : SVG_PATH_HOLLOW
+        )
 
         container.addEventListener("click", () => {
           watchLaterCheckbox?.click()
 
-          container.style.color = watchLaterCheckbox?.checked
-            ? ACTIVE_STYLE
-            : INACTIVE_BUTTON_STYLE
-          text.style.color = watchLaterCheckbox?.checked
-            ? ACTIVE_STYLE
-            : INACTIVE_TEXT_STYLE
+          path.setAttribute(
+            "d",
+            watchLaterCheckbox?.checked ? SVG_PATH_FILLED : SVG_PATH_HOLLOW
+          )
         })
       }
     }
