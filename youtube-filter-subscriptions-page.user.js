0dd7f9f7a251b093e233ca11e5bb7243f2d7fdd6
Zach Hardesty <hello@zachhardesty.com>
2025-11-02 17:32:49 -0600

    fix(yt): update filter subs to use new selectors

--- youtube-filter-subscriptions-page.user.js
+++ youtube-filter-subscriptions-page.user.js
@@ -2,10 +2,10 @@
 // @name         YouTube - Filter Subscriptions Page
 // @namespace    https://zachhardesty.com
 // @author       Zach Hardesty <zachhardesty7@users.noreply.github.com> (https://github.com/zachhardesty7)
-// @description  hide videos with given title keywords
-// @copyright    2019-2024, Zach Hardesty (https://zachhardesty.com/)
+// @description  hide videos with given keywords in title on YouTube home and subscriptions pages
+// @copyright    2019-2025, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      2.1.0
+// @version      2.2.0
 
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -21,14 +21,32 @@
 // @downloadURL  https://openuserjs.org/src/scripts/zachhardesty7/YouTube_-_Filter_Subscriptions_Page.user.js
 
 // @match        https://www.youtube.com/*
-// @require      https://greasyfork.org/scripts/419640-onelementready/code/onElementReady.js?version=887637
+// @require      https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/refs/tags/onElementReady@0.10.0/utils/onElementReady.js
 // ==/UserScript==
 
 /* global onElementReady */
 
 const HIDDEN_CLASSNAME = "zh-hidden"
+const GM_FILTERS_KEY = "yt-filter-page"
 
-let keywords = GM_getValue("yt-filter-page", "")
+/** for `youtube.com` home page */
+const SELECTOR_HOME_PAGE = 'ytd-browse[page-subtype="home"]'
+/** for `youtube.com/feed/subscriptions` page */
+const SELECTOR_SUBS_PAGE = 'ytd-browse[page-subtype="subscriptions"]'
+
+/** for grid view */
+const SELECTOR_GRID_ITEM = "ytd-rich-item-renderer.ytd-rich-grid-renderer"
+const SELECTOR_GRID_ITEM_TITLE = "a.yt-lockup-metadata-view-model__title"
+
+/**
+ * for list view
+ *
+ * @todo - incorrectly selects shorts
+ */
+const SELECTOR_LIST_ITEM = `ytd-item-section-renderer.ytd-section-list-renderer`
+const SELECTOR_LIST_ITEM_TITLE = `#video-title yt-formatted-string`
+
+let keywords = GM_getValue(GM_FILTERS_KEY, "")
 
 const stylesheet = document.createElement("style")
 const head = document.head || document.querySelectorAll("head")[0]
@@ -43,45 +61,22 @@ stylesheet.append(
 )
 head.append(stylesheet)
 
-// runs on youtube.com/feed/subscriptions
 onElementReady(
-  "#dismissible.style-scope.ytd-grid-video-renderer",
+  `:is(${SELECTOR_HOME_PAGE}, ${SELECTOR_SUBS_PAGE}) :is(${SELECTOR_GRID_ITEM}, ${SELECTOR_LIST_ITEM})`,
   { findOnce: false },
   (el) => {
     const videoTitle = el
-      .querySelector("#details #meta")
-      .firstElementChild.textContent.toLowerCase()
-    const parentContainer = el.parentElement
+      ?.querySelector(`:is(${SELECTOR_GRID_ITEM_TITLE}, ${SELECTOR_LIST_ITEM_TITLE})`)
+      ?.textContent.toLowerCase()
 
     for (const keyword of keywords.toLowerCase().split(",")) {
-      if (keyword && videoTitle.includes(keyword)) {
-        parentContainer.classList.add(HIDDEN_CLASSNAME)
+      if (keyword && videoTitle?.includes(keyword)) {
+        el.classList.add(HIDDEN_CLASSNAME)
         return
       }
     }
 
-    parentContainer.classList.remove(HIDDEN_CLASSNAME)
-  },
-)
-
-// runs on youtube.com
-onElementReady(
-  "#dismissible.style-scope.ytd-rich-grid-media",
-  { findOnce: false },
-  (el) => {
-    const videoTitle = el
-      .querySelector("#details #meta")
-      .firstElementChild.textContent.toLowerCase()
-    const parentContainer = el.parentElement.parentElement.parentElement
-
-    for (const keyword of keywords.toLowerCase().split(",")) {
-      if (keyword && videoTitle.includes(keyword)) {
-        parentContainer.classList.add(HIDDEN_CLASSNAME)
-        return
-      }
-    }
-
-    parentContainer.classList.remove(HIDDEN_CLASSNAME)
+    el.classList.remove(HIDDEN_CLASSNAME)
   },
 )
 
@@ -94,6 +89,6 @@ GM_registerMenuCommand("Set YT Filter Subscriptions Page Keywords", () => {
 
   if (val !== null) {
     keywords = val
-    GM_setValue("yt-filter-page", val)
+    GM_setValue(GM_FILTERS_KEY, val)
   }
 })
