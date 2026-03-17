48550fa66093596a49fff1d9f2c92efd3e9a1b19
Zach Hardesty <hello@zachhardesty.com>
2026-03-17 17:34:24 -0500

    feat(soundcloud): fix selectors, use 1 OER

--- soundcloud-download-button.user.js
+++ soundcloud-download-button.user.js
@@ -5,7 +5,7 @@
 // @description  adds a button on main page and song page to download song automatically from https://soundcloudmp3.org/
 // @copyright    2019-2026, Zach Hardesty (https://zachhardesty.com/)
 // @license      GPL-3.0-only; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      2.1.3
+// @version      2.2.0
 
 // @homepageURL  https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/soundcloud-download-button.user.js
 // @homepage     https://github.com/zachhardesty7/tamper-monkey-scripts-collection/raw/master/soundcloud-download-button.user.js
@@ -23,6 +23,18 @@
 
 /* global onElementReady */
 
+// #region - test page links (to add external download button to)
+// song: https://soundcloud.com/user-342593999-672340495/the-quietude
+// song (w/ existing download icon): https://soundcloud.com/neilcic/mouth-sounds
+// profile (w/ sets, w/ & w/o existing download icon): https://soundcloud.com/noisia
+// set: https://soundcloud.com/noisia/sets/the-resonance-vii
+// feed: https://soundcloud.com/feed
+// #endregion
+
+const SELECTOR_SONG_LIST_ITEM = ".soundList__item"
+// FIXME: triggers twice for some reason, but still seems to work fine
+const SELECTOR_SONG_PAGE = ".listenEngagement__footer"
+
 /**
  * adds button to soundcloud
  *
@@ -33,20 +45,21 @@ function addSoundcloudDownloadButton(el) {
   const button = document.createElement("button")
   button.textContent = "External Download"
 
-  // if on soundcloud home and song node is not a playlist, append SC styled button
+  // if on soundcloud home feed and song node is not a playlist, append small SC styled button
   if (
-    link.includes("stream") &&
+    el.matches(SELECTOR_SONG_LIST_ITEM) &&
     !(
-      /** @type {HTMLAnchorElement} */ (
+      /** @type {HTMLAnchorElement | null} */ (
         el.querySelector(".soundTitle__title")
-      ).href.includes("/sets/")
+      )?.href.includes("/sets/")
     )
   ) {
     button.className = "mp3-button sc-button sc-button-small"
     el.querySelector(".soundActions .sc-button-group")?.append(button)
+
     // else if on individual song page (and not playlist), append SC styled button
   } else if (
-    !link.includes("stream") &&
+    el.matches(SELECTOR_SONG_PAGE) &&
     (!link.includes("/sets/") || link.includes("?in="))
   ) {
     const toolbar = document.querySelector(".soundActions div:first-child")
@@ -59,18 +72,22 @@ function addSoundcloudDownloadButton(el) {
   })
 }
 
-// auto-run on soundcloud mp3
-// if referred from soundcloud, grab data from GM storage
-// paste and submit to begin conversion to mp3
 function mp3() {
   const { href } = window.location
   if (href.includes("loader.to")) {
+    // auto-run download when page loads soundcloud mp3
     onElementReady(
       "#ds .card .section:last-of-type > progress",
       { findOnce: false },
       (progress) => {
         const timer = setInterval(() => {
-          if (/** @type {HTMLProgressElement} */ (progress).value === 1000) {
+          if (!(progress instanceof HTMLProgressElement)) {
+            clearInterval(timer)
+            throw new TypeError("soundcloud-download-button: expected progress element")
+          }
+
+          if (progress.value === 1000) {
+            /** @type {HTMLAnchorElement | null} */
             const button = document.querySelector("#ds .card .section:last-of-type > a")
             button?.click()
             clearInterval(timer)
@@ -80,13 +97,8 @@ function mp3() {
     )
   } else {
     onElementReady(
-      ".l-listen-wrapper",
-      { findOnce: false },
-      addSoundcloudDownloadButton,
-    )
-    onElementReady(
-      ".lazyLoadingList__list > .soundList__item",
-      { findOnce: false },
+      `:is(${SELECTOR_SONG_LIST_ITEM}, ${SELECTOR_SONG_PAGE})`,
+      { findOnce: true },
       addSoundcloudDownloadButton,
     )
   }
